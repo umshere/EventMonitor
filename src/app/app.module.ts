93bf7be5557ffeec1166e986fa42de3e4adfb913
Chathoth <umesh.m.chathoth@accenture.com>
2019-04-23 22:46:30 -0700

    integrated MDB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { SubHeaderComponent } from './sub-header/sub-header.component';
@@ -16,7 +16,8 @@ import { SubheaderRightComponent } from './sub-header/subheader-right/subheader-
     SubheaderRightComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
